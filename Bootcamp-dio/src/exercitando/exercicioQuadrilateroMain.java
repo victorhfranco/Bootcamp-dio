259932f62bb2425c78c024ff79aa4819fa999381
Victor Henrique Franco <victor.hfranco@yahoo.com>
2022-05-05 16:14:26 -0300

    Exercicio sobrecarga

---
+++ Bootcamp-dio/src/exercitando/exercicioQuadrilateroMain.java
@@ -0,0 +1,14 @@
+package exercitando;
+
+public class exercicioQuadrilateroMain {
+	public static void main(String[] args) {
+		
+		//Quadrilatéro
+		System.out.println("Exercicio quadrilátero");
+		exercicioQuadrilatero.area(3);
+		exercicioQuadrilatero.area(5d, 5d);
+		exercicioQuadrilatero.area(7, 8, 9);
+		exercicioQuadrilatero.area(5f, 5f);
+		
+	}
+}	
